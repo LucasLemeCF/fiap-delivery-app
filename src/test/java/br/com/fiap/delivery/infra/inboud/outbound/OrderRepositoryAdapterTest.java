6b55a1e354a27481304755c8d091a76cfa56fde8
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2024-12-03 21:14:42 -0300

    Fix test

---
+++ src/test/java/br/com/fiap/delivery/infra/inboud/outbound/OrderRepositoryAdapterTest.java
@@ -0,0 +1,84 @@
+package br.com.fiap.delivery.infra.inboud.outbound;
+
+import br.com.fiap.delivery.core.domain.OrderDomain;
+import br.com.fiap.delivery.core.domain.enums.OrderStatus;
+import br.com.fiap.delivery.infra.entities.OrderEntity;
+import br.com.fiap.delivery.infra.outbound.OrderRepositoryAdapter;
+import br.com.fiap.delivery.infra.outbound.repositories.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderRepositoryAdapterTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private OrderRepositoryAdapter orderRepositoryAdapter;
+
+    @Test
+    void testCreateOrderSuccess() {
+        Long orderId = 1L;
+        String customerName = "Test Customer";
+        BigDecimal price = BigDecimal.valueOf(100.00);
+        OrderStatus status = OrderStatus.RECEIVED;
+
+        LocalDateTime creationAt = LocalDateTime.now(); // Capture the actual time
+
+        OrderDomain order = new OrderDomain(orderId, customerName, creationAt, price, status, null);
+        OrderEntity expectedEntity = new OrderEntity(orderId, customerName, creationAt, price, status, null);
+
+        when(orderRepository.save(expectedEntity)).thenReturn(expectedEntity);
+
+        OrderDomain createdOrder = orderRepositoryAdapter.createOrder(order);
+
+        assertEquals(order, createdOrder);
+    }
+
+    @Test
+    void testSearchBySuccess() {
+        Long orderId = 1L;
+        OrderEntity expectedEntity = new OrderEntity(orderId, "Test Customer", LocalDateTime.now(), BigDecimal.ZERO, OrderStatus.RECEIVED, null);
+
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(expectedEntity));
+
+        orderRepositoryAdapter.searchBy(orderId);
+
+        verify(orderRepository).findById(orderId);
+    }
+
+    @Test
+    void testUpdateSuccess() {
+        Long orderId = 1L;
+        String initialCustomerName = "Old Customer";
+        String updatedCustomerName = "Updated Customer";
+        BigDecimal initialPrice = BigDecimal.ZERO;
+        BigDecimal updatedPrice = BigDecimal.valueOf(100.00);
+        OrderStatus initialStatus = OrderStatus.RECEIVED;
+        OrderStatus updatedStatus = OrderStatus.WAITING_PAYMENT;
+
+        LocalDateTime creationAt = LocalDateTime.now();
+
+        OrderDomain initialOrder = new OrderDomain(orderId, initialCustomerName, creationAt, initialPrice, initialStatus, null);
+        OrderEntity initialEntity = new OrderEntity(orderId, initialCustomerName, creationAt, initialPrice, initialStatus, null);
+        OrderEntity updatedEntity = new OrderEntity(orderId, updatedCustomerName, creationAt, updatedPrice, updatedStatus, null);
+
+        when(orderRepository.save(initialEntity)).thenReturn(updatedEntity);
+
+        orderRepositoryAdapter.update(initialOrder);
+
+        verify(orderRepository).save(initialEntity);
+    }
+}
---
+++ src/test/java/br/com/fiap/delivery/infra/inboud/outbound/ProductRepositoryAdapterTest.java
@@ -0,0 +1,88 @@
+package br.com.fiap.delivery.infra.inboud.outbound;
+
+import br.com.fiap.delivery.core.domain.CategoryDomain;
+import br.com.fiap.delivery.core.domain.ProductDomain;
+import br.com.fiap.delivery.core.domain.exceptions.NotFoundException;
+import br.com.fiap.delivery.infra.entities.CategoryEntity;
+import br.com.fiap.delivery.infra.entities.ProductEntity;
+import br.com.fiap.delivery.infra.outbound.ProductRepositoryAdapter;
+import br.com.fiap.delivery.infra.outbound.repositories.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductRepositoryAdapterTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductRepositoryAdapter adapter;
+
+    @Test
+    public void testSearchProductSuccess() throws NotFoundException {
+        Long expectedId = 1L;
+        String expectedName = "Test Product";
+        String expectedDescription = "This is a test product";
+        BigDecimal expectedValue = BigDecimal.valueOf(10.00);
+        List<ProductEntity> expectedProducts = new ArrayList<>();
+        CategoryEntity expectedCategory = new CategoryEntity(1L, "Test Product", expectedProducts);
+        boolean expectedIsActive = true;
+
+        ProductEntity expectedEntity = new ProductEntity(expectedId, expectedName, expectedDescription, expectedValue, expectedCategory, expectedIsActive);
+
+        when(productRepository.findByName(expectedName)).thenReturn(expectedEntity);
+
+        ProductDomain actualProduct = adapter.searchProduct("Test Product");
+
+        assertNotNull(actualProduct);
+        assertEquals(expectedId, actualProduct.getId());
+        assertEquals(expectedName, actualProduct.getName());
+
+        verify(productRepository).findByName(expectedName);
+    }
+
+    @Test
+    public void testCreateSuccess() {
+        Long expectedId = 1L;
+        String expectedName = "Test Product";
+        String expectedDescription = "This is a test product";
+        BigDecimal expectedValue = BigDecimal.valueOf(10.00);
+        List<ProductEntity> expectedProducts = new ArrayList<>();
+        boolean expectedIsActive = true;
+
+        CategoryDomain expectedCategoryDomain = new CategoryDomain(1L, "Electronics");
+        CategoryEntity expectedCategoryEntity = new CategoryEntity(1L, "Doesn't Matter", expectedProducts);
+        ProductEntity expectedEntity = new ProductEntity(null, expectedName, expectedDescription, expectedValue, expectedCategoryEntity, expectedIsActive);
+
+        when(productRepository.save(argThat(entity -> entity.getCategory().getId().equals(expectedCategoryDomain.getId())))).thenReturn(expectedEntity);
+
+        ProductRepositoryAdapter adapter = new ProductRepositoryAdapter(productRepository);
+        ProductDomain newProduct = new ProductDomain(
+                null,
+                "Smartphone Top de Linha",
+                "Smartphone com as melhores especificações do mercado.",
+                BigDecimal.valueOf(5000.00),
+                new CategoryDomain(1L, "Eletrônicos") ,
+                true
+        );
+
+        ProductDomain createdProduct = adapter.create(newProduct);
+
+        assertNotNull(createdProduct);
+    }
+
+}
